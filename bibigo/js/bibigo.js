214489fc4d4536824e9f6ab5c31dcb051a83eb78
juhee067 <66045537+juhee067@users.noreply.github.com>
2022-12-13 23:35:24 +0900

    20221213

--- bibigo/js/bibigo.js
+++ bibigo/js/bibigo.js
@@ -1,3 +1,14 @@
+window.onload = function () {
+  let search = document.querySelector("#SCBtn");
+  let searchBar = document.querySelector("#searchBar");
+  let close = document.querySelector("#close");
+  search.addEventListener("click", function () {
+    searchBar.style.display = "block";
+  });
+  close.addEventListener("click", function () {
+    searchBar.style.display = "none";
+  });
+};
 AOS.init();
 
 $(".slider").slick({
@@ -6,11 +17,15 @@ $(".slider").slick({
   autoplay: true,
 });
 
-window.onload = function () {
-  let search = document.getElementById("#search");
-  let searchBar = document.getElementsByClassName("searchBar")[0];
-  search.addEventListener("click", function () {
-    console.log("aa");
-    // searchBar.style.display = "block";
-  });
-};
+// scroll
+window.addEventListener("scroll", function () {
+  let scrollItem = document.querySelector(".scrollUp");
+  if (window.scrollY >= 450) {
+    scrollItem.classList.add("on");
+    scrollItem.innerHTML = '<i class="xi-arrow-up"></i>';
+  } else {
+    scrollItem.classList.remove("on");
+    scrollItem.innerHTML = 'SCROLL DOWN<i class="xi-angle-down-min"></i>';
+  }
+  console.log(scrollItem.className);
+});
